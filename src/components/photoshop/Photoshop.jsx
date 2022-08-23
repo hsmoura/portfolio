fa8ddd2abdf2274d499f84483aaadb3371a20387
hsmoura <32681606+hsmoura@users.noreply.github.com>
2022-08-24 00:06:56 +0100

    word fixes

--- src/components/photoshop/Photoshop.jsx
+++ src/components/photoshop/Photoshop.jsx
@@ -6,7 +6,7 @@ import { products } from '../../data'
 export default function Photoshop() {
   return (
     <div className="photoshop" id="photoshop">
-        <h2>Photoshop Galery</h2>
+        <h2>Photoshop Gallery</h2>
         <p>Sometimes in my free time I like to explore Photoshop and After Effects to create digital art.</p>
         <div className="clist">
             {products.map(item=>(
